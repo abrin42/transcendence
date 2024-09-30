08d8584082c7d928ef3a6b8b3e7fe9b69bbdb087
Lucas Ribette <lucasribette.pro@gmail.com>
2024-09-30 15:17:22 +0200

    Quick fix during the merge

--- django/pong/settings.py
+++ django/pong/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = get_secret('secret_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['api']
 #ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'yourdomain.com']
 
 
